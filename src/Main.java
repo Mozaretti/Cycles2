3c6192014cd099a4c0385cc7f5861b17b580ff98
Eвгений <mocard778@gmail.com>
2023-02-12 20:07:29 +0700

    2 задача

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
+        task3();
     }
     public static void task1(){
         System.out.println("Задача 1");
@@ -12,5 +14,23 @@ public class Main {
             System.out.println("Месяц"+ month +" , сумма накоплений равна"+ sum + " рублей");
             month++;
         }
+        System.out.println();
+    }
+    public static void task2(){
+        System.out.println("Задача 2");
+        int num = 1;
+        while (num <= 10){
+            System.out.print(num++ + " " );
+            }
+        System.out.println();
+        int number = 10;
+        for (int i = 10; i >= 1; i = i - 1){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+    }
+    public static void task3(){
+        System.out.println("Задача 3");
     }
 }
