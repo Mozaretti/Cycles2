49809cc673b9d62cb6dd15362203328236af01a0
Eвгений <mocard778@gmail.com>
2023-02-13 17:50:55 +0700

    5 задача

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
+        task5();
     }
     public static void task1(){
         System.out.println("Задача 1");
@@ -40,5 +42,31 @@ public class Main {
             System.out.println("Год " + year + " , численность населения составляет "+ population);
         }
 
+    }
+
+    public static void task4(){
+        System.out.println("Задача 4");
+        int sum = 15000;
+        int month = 1;
+        while (sum < 12_000_000){
+            sum = sum + sum * 7 / 100;
+            System.out.println("Месяц " + month + " , сумма ." + sum);
+            month++;
+        }
+        System.out.println();
+    }
+    public static void task5() {
+        System.out.println("Задача 5");
+        int sum = 15000;
+        int month = 1;
+        while (sum < 12_000_000){
+            sum = sum + sum * 7 / 100;
+            month++;
+            if (month % 6 == 0) {
+                System.out.print("Месяц " + month + " , сумма " + sum);
+                System.out.println();
+            }
+        }
+        System.out.println(month + " суммарное количество месяцев");
     }
 }
